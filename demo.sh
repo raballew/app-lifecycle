6a7579d6b5c5120015e5e81ec89b4ffa2065701e
Paul Wallrabe <pwallrab@redhat.com>
2023-06-07 16:15:49 +0200

    wip

--- demo.sh
+++ demo.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# include the magic
+# todo: fix digests being different
+# todo: configure systemd to used /var/quadlets
+# todo: selinux policies
+
 . magic.sh
 TYPE_SPEED=80
 
-# hide the evidence
 clear
+
+ROOT_DIR=/var
 
 get_image_id() {
     if [[ $# -eq 0 ]] ; then
@@ -43,6 +47,7 @@ create_tarball_from_dir() {
         cp "$f" $2/$digest"_"$config_checksum
     done
 
+    podman image prune --all -f
     archive=$(basename $1)
     touch $2/$archive.tar.gz
     sync
@@ -51,9 +56,8 @@ create_tarball_from_dir() {
 }
 
 cleanup() {
-    used_digests=$(cat /var/quadlets/* | grep -oP '(?<=@sha256:).*')
-    for dir in /var/apps/*/ ; do
-        echo "dddd: $dir"
+    used_digests=$(cat $ROOT_DIR/quadlets/* | grep -oP '(?<=@sha256:).*')
+    for dir in $ROOT_DIR/apps/*/ ; do
         found=false
         for digest in $used_digests ; do
             if [[ $dir =~ "$digest" ]]; then
@@ -66,19 +70,21 @@ cleanup() {
             rm -rf $dir
         fi
     done
+
+    find $ROOT_DIR/ -xtype l -delete
 }
 
 install_tarball() {
     d=$(dirname $1)
     (cd $d && sha256sum -c $1.sig)
-    tar -zxvf $1 -C /var/apps/
+    tar -zxvf $1 -C $ROOT_DIR/apps/
     transaction_checksum=$(cat $1.sig | cut -d " " -f 1)
-    transaction_dir=/var/quadlet_$transaction_checksum
+    transaction_dir=$ROOT_DIR/quadlet_$transaction_checksum
     mkdir -p $transaction_dir
 
     # link old configs
-    link=$(readlink -f /var/quadlets)
-    if [[ ! "$link" == "/var/quadlets" && ! -z "$link" ]] ; then
+    link=$(readlink -f $ROOT_DIR/quadlets)
+    if [[ ! "$link" == "$ROOT_DIR/quadlets" && ! -z "$link" ]] ; then
         for f in $link/* ; do
             [ -e "$f" ] || continue
             src=$(find -L "$f" | tail -n1)
@@ -90,7 +96,7 @@ install_tarball() {
     # copy new configs and replace overwritten old ones
     config_dirs=$(tar -ztf $1 | tree --fromfile . -L 2 -d -if | grep "_" | sed "s#./##g")
     for dd in $config_dirs ; do
-        for f in /var/apps/$dd/* ; do
+        for f in $ROOT_DIR/apps/$dd/* ; do
             [ -e "$f" ] || continue
             src=$(find -L "$f" | tail -n1)
             name=$(basename "$src")
@@ -100,17 +106,29 @@ install_tarball() {
 
     image_dirs=$(tar -ztf $1 | tree --fromfile . -L 2 -d -if | grep -v "_" | sed "s#\/##g" | sed "s#\.##g" | grep -v "directories")
 
-    # podman load -i path/in/tar/digest + podman tag from index.json annotation
-    ln -v -snf $transaction_dir /var/quadlets
+    for dir in $image_dirs ; do
+        mkdir -p $ROOT_DIR/apps/$dir/imagestore
+        podman --root=$ROOT_DIR/apps/$dir/imagestore load -i $ROOT_DIR/apps/$dir
+        sync
+        target_name=$(jq -r '.manifests[0].annotations."org.opencontainers.image.ref.name"' $ROOT_DIR/apps/$dir/index.json | sed 's/\@.*//')
+        podman --root=$ROOT_DIR/apps/$dir/imagestore tag localhost$ROOT_DIR/apps/$dir $target_name
+        find $ROOT_DIR/apps/$dir -mindepth 1 ! -regex "^$ROOT_DIR/apps/$dir/imagestore\(/.*\)?" -delete
+        mv $ROOT_DIR/apps/$dir/imagestore/* /path/
+        mv -f $ROOT_DIR/apps/$dir/imagestore/{.,}* $ROOT_DIR/apps/$dir/
+        rm -rf $ROOT_DIR/apps/$dir/imagestore/
+    done
+
+    ln -v -snf $transaction_dir $ROOT_DIR/quadlets
 
     cleanup
+    systemctl daemon-reload
 }
 
-rm -rf /var/apps/
-rm -rf /var/quadlets/
-rm -rf /var/quadlet_*/
+rm -rf $ROOT_DIR/apps/
+rm -rf $ROOT_DIR/quadlets/
+rm -rf $ROOT_DIR/quadlet_*/
 rm -rf /tmp/tmp.*/
-mkdir -p /var/apps/
+mkdir -p $ROOT_DIR/apps/
 
 # configure systemd to load quadlet files
 
@@ -124,21 +142,21 @@ b_tmp=$(mktemp -d)
 create_tarball_from_dir "apps/b" $b_tmp >/dev/null 2>&1
 pei "tar -ztf $b_tmp/b.tar.gz | tree --fromfile ."
 
-pe "# 2 - show current directory structure"
-pei "ls -lL /var/apps/"
-pei "ls -lL /var/quadlet_*"
-pei "ls -lL /var/quadlets"
+pei "# 2 - show current directory structure"
+pei "ls -lL $ROOT_DIR/apps/"
+pei "ls -lL $ROOT_DIR/quadlet_*"
+pei "ls -lL $ROOT_DIR/quadlets"
 
 pe "# 3 - install tarball A"
 install_tarball $a_tmp/a.tar.gz >/dev/null 2>&1
-pei "# used container image digests in /var/quadlets"
-pei "cat /var/quadlets/* | grep "Image=" | sed s#Image=##g"
-pei "# directories in /var/apps/"
-pei "find /var/apps/ -maxdepth 1 | grep -v "_" | sed s#/var/apps/##g"
+pei "# used container image digests in $ROOT_DIR/quadlets"
+pei "cat $ROOT_DIR/quadlets/* | grep "Image=" | sed s#Image=##g"
+pei "# directories in $ROOT_DIR/apps/"
+pei "find $ROOT_DIR/apps/ -maxdepth 1 | grep -v "_" | sed s#$ROOT_DIR/apps/##g"
 
 pe "# 4 - install tarball B"
 install_tarball $b_tmp/b.tar.gz >/dev/null 2>&1
-pei "# used container image digests in /var/quadlets"
-pei "cat /var/quadlets/* | grep "Image=" | sed s#Image=##g"
-pei "# directories in /var/apps/"
-pei "find /var/apps/ -maxdepth 1 | grep -v "_" | sed s#/var/apps/##g"
+pei "# used container image digests in $ROOT_DIR/quadlets"
+pei "cat $ROOT_DIR/quadlets/* | grep "Image=" | sed s#Image=##g"
+pei "# directories in $ROOT_DIR/apps/"
+pei "find $ROOT_DIR/apps/ -maxdepth 1 | grep -v "_" | sed s#$ROOT_DIR/apps/##g"
